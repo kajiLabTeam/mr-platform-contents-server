4d956f75c82bc77fb3a805365ba8677ae5740595
kanakanho <kahoshibakichi@gmail.com>
2024-06-27 19:46:05 +0900

    feat: apiを立てる,sqlの初期化

---
+++ src/router/router.go
@@ -0,0 +1,27 @@
+package router
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func Init() {
+	f, _ := os.Create("../log/server.log")
+	gin.DefaultWriter = io.MultiWriter(f, os.Stdout)
+
+	r := gin.Default()
+	r.Use(cors.Default())
+	r.GET("/hello", func(c *gin.Context) {
+		c.String(http.StatusOK, "Hello World!!")
+	})
+	if err := r.Run("0.0.0.0:8000"); err != nil {
+		fmt.Println("サーバーの起動に失敗しました:", err)
+	} else {
+		fmt.Println("サーバーが正常に起動しました")
+	}
+}
