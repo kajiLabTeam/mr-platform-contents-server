147ed72268408f39f0f1e7a0c9468d216fc5b255
kanakanho <kahoshibakichi@gmail.com>
2024-08-08 14:29:35 +0900

    chnage: logテーブルの追加、rowの綴り、コンテンツの存在確認の不一致の修正

--- src/model/html2d.go
+++ src/model/html2d.go
@@ -8,10 +8,10 @@ import (
 )
 
 func GetHtml2dContent(contentId string) (common.Html2d, error) {
-	roll := db.QueryRow("SELECT location_x, location_y, location_z, rotation_roll, rotation_pitch, rotation_yaw, size_width, size_height, text_type, text_url, style_url FROM html2d WHERE content_id = $1", contentId)
+	row := db.QueryRow("SELECT location_x, location_y, location_z, rotation_roll, rotation_pitch, rotation_yaw, size_width, size_height, text_type, text_url, style_url FROM html2d WHERE content_id = $1", contentId)
 
 	var content common.Html2d
-	if err := roll.Scan(&content.Location.X, &content.Location.Y, &content.Location.Z, &content.Rotation.Roll, &content.Rotation.Pitch, &content.Rotation.Yaw, &content.Size.Width, &content.Size.Height, &content.TextType, &content.TextURL, &content.StyleURL); err != nil {
+	if err := row.Scan(&content.Location.X, &content.Location.Y, &content.Location.Z, &content.Rotation.Roll, &content.Rotation.Pitch, &content.Rotation.Yaw, &content.Size.Width, &content.Size.Height, &content.TextType, &content.TextURL, &content.StyleURL); err != nil {
 		return common.Html2d{}, err
 	}
 	return content, nil
@@ -24,16 +24,21 @@ func CreateHtml2dContent(contentId string, content common.Html2d) error {
 	if err != nil {
 		return err
 	}
+	// ログテーブルにも追加
+	if err := insertLogHtml2dContent(uuid.String(), contentId, content); err != nil {
+		return err
+	}
+	
 	return nil
 }
 
 func IsExistHtml2dContent(contentId string) (bool, error) {
-	roll := db.QueryRow("SELECT location_x, location_y, location_z, rotation_roll, rotation_pitch, rotation_yaw, size_width, size_height,text_type, text_url, style_url FROM html2d WHERE content_id = $1", contentId)
+	row := db.QueryRow("SELECT location_x, location_y, location_z, rotation_roll, rotation_pitch, rotation_yaw, size_width, size_height,text_type, text_url, style_url FROM html2d WHERE content_id = $1", contentId)
 
 	var content common.SQLHtml2d
-	if err := roll.Scan(&content.Location.X, &content.Location.Y, &content.Location.Z, &content.Rotation.Roll, &content.Rotation.Pitch, &content.Rotation.Yaw, &content.Size.Width, &content.Size.Height, &content.TextType, &content.TextURL, &content.StyleURL); err != nil {
+	if err := row.Scan(&content.Location.X, &content.Location.Y, &content.Location.Z, &content.Rotation.Roll, &content.Rotation.Pitch, &content.Rotation.Yaw, &content.Size.Width, &content.Size.Height, &content.TextType, &content.TextURL, &content.StyleURL); err != nil {
 		if err == sql.ErrNoRows {
-			// No rolls were returned, return false and no error
+			// No rows were returned, return false and no error
 			return false, nil
 		}
 		return false, err
@@ -43,15 +48,15 @@ func IsExistHtml2dContent(contentId string) (bool, error) {
 
 // id以外を比較して同一のコンテンツが存在するか確認
 func IsExistHtml2dContentExceptId(checkContent common.Html2d) (bool, error) {
-	roll := db.QueryRow("SELECT location_x, location_y, location_z, rotation_roll, rotation_pitch, rotation_yaw, size_width, size_height,text_type, text_url, style_url FROM html2d WHERE location_x = $1 AND location_y = $2 AND location_z = $3 AND rotation_roll = $4 AND rotation_pitch = $5 AND rotation_yaw = $6 AND size_width = $7 AND size_height = $8 AND text_type = $9 AND text_url = $10 AND style_url = $11", checkContent.Location.X, checkContent.Location.Y, checkContent.Location.Z, checkContent.Rotation.Roll, checkContent.Rotation.Pitch, checkContent.Rotation.Yaw, checkContent.Size.Width, checkContent.Size.Height, checkContent.TextType, checkContent.TextURL, checkContent.StyleURL)
+	row := db.QueryRow("SELECT location_x, location_y, location_z, rotation_roll, rotation_pitch, rotation_yaw, size_width, size_height, text_type, text_url, style_url FROM html2d WHERE location_x = $1 AND location_y = $2 AND location_z = $3 AND rotation_roll = $4 AND rotation_pitch = $5 AND rotation_yaw = $6 AND size_width = $7 AND size_height = $8 AND text_type = $9 AND text_url = $10 AND style_url = $11", checkContent.Location.X, checkContent.Location.Y, checkContent.Location.Z, checkContent.Rotation.Roll, checkContent.Rotation.Pitch, checkContent.Rotation.Yaw, checkContent.Size.Width, checkContent.Size.Height, checkContent.TextType, checkContent.TextURL, checkContent.StyleURL)
 
 	var content common.SQLHtml2d
-	if err := roll.Scan(&content.Location.X, &content.Location.Y, &content.Location.Z, &content.Rotation.Roll, &content.Rotation.Pitch, &content.Rotation.Yaw, &content.Size.Width, &content.Size.Height, &content.TextType, &content.TextURL, &content.StyleURL); err != nil {
-		if err == sql.ErrNoRows {
-			// No rolls were returned, return false and no error
-			return false, nil
-		}
-		return false, err
+	if err := row.Scan(&content.Location.X, &content.Location.Y, &content.Location.Z, &content.Rotation.Roll, &content.Rotation.Pitch, &content.Rotation.Yaw, &content.Size.Width, &content.Size.Height, &content.TextType, &content.TextURL, &content.StyleURL); err != nil {
+			if err == sql.ErrNoRows {
+					// No rows were returned, return false and no error
+					return false, nil
+			}
+			return false, err
 	}
 	return true, nil
 }
@@ -61,5 +66,35 @@ func UpdateHtml2dContent(contentId string, content common.Html2d) (bool, error) 
 	if err != nil {
 		return false, err
 	}
+
+	// ログテーブルにも追加
+	html2dId, err := getHtml2dId(contentId)
+	if err != nil {
+		return false, err
+	}
+
+	if err := insertLogHtml2dContent(html2dId, contentId, content); err != nil {
+		return false, err
+	}
 	return true, nil
+}
+
+func getHtml2dId(contentId string) (string, error) {
+	row := db.QueryRow("SELECT id FROM html2d WHERE content_id = $1", contentId)
+
+	var html2dId string
+	if err := row.Scan(&html2dId); err != nil {
+		return "", err
+	}
+	return html2dId, nil
+}
+
+func insertLogHtml2dContent(html2d_id string, contentId string, content common.Html2d) error {
+	// uuidを生成
+	uuid := uuid.New()
+	_, err := db.Exec("INSERT INTO log_html2d (id, html2d_id, content_id, location_x, location_y, location_z, rotation_roll, rotation_pitch, rotation_yaw, size_width, size_height, text_type, text_url, style_url) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)", uuid.String(), html2d_id, contentId, content.Location.X, content.Location.Y, content.Location.Z, content.Rotation.Roll, content.Rotation.Pitch, content.Rotation.Yaw, content.Size.Width, content.Size.Height, content.TextType, content.TextURL, content.StyleURL)
+	if err != nil {
+		return err
+	}
+	return nil
 }
